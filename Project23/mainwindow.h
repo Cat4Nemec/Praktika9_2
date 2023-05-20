14f93bc8fea4e9c9c5256d7c093919c8a5686f8c
Цайтлер Данил < @WIN-93MP03L6MQV>
2023-05-20 19:05:41 +0400

    Добавьте файлы проекта.

---
+++ Project23/mainwindow.h
@@ -0,0 +1,22 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QLabel>
+#include <QTime>
+#include <QApplication>
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget* parent = nullptr);
+    ~MainWindow();
+    void Ftimer();
+
+private:
+    QLabel* timer;
+};
+#endif
+#pragma once
